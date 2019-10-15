e20a6b1606ad2affa7d459c5b0b6825d0e30d53d
ArqTras <33489188+ArqTras@users.noreply.github.com>
2019-10-15 21:04:05 +0200

    Incoming RandomARQ config

--- website_example/config.js
+++ website_example/config.js
@@ -1,14 +1,13 @@
 var api = "http://poolhost:8117";
 
-var email = "support@poolhost.com";
-var telegram = "https://t.me/YourPool";
-var discord = "https://discordapp.com/invite/YourPool";
-var facebook = "https://www.facebook.com/<YourPoolFacebook>";
+var email = "support@arqma.com";
+var telegram = "https://t.me/joinchat/HNpOMRIiNSoCn0zYrAOofw";
+var discord = "https://chat.arqma.com";
 
-var marketCurrencies = ["{symbol}-BTC", "{symbol}-LTC", "{symbol}-DOGE", "{symbol}-USDT", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "http://stageblocks.arqma.com/block/{id}";
+var transactionExplorer = "http://stageblocks.arqma.com/tx/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
